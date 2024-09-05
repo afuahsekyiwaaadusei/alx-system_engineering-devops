c692b5092c30b501d390d72150989691ef1bac04
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-09-05 14:54:13 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-import sys
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers in the provided subreddit.
+    Returns 0 if the subreddit does not exist
+    """
+    import requests
+    import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    custom_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,\
+         like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    headers = {'user-agent': custom_agent}
 
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 404:  # non-existent subreddit
+        return 0
+    r_data = r.json().get('data')
+    return r_data.get('subscribers')
