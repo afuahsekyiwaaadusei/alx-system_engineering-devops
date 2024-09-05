8b2cee2a98932a1eebfb1a2866279e9ec2763cdb
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-09-05 16:45:35 +0000

    Task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,24 @@
+f
 #!/usr/bin/python3
-"""Contains top_ten function"""
+"""function that prints the
+titles of the top 10 hottest posts a given subreddit."""
+
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code != 200:
+    """Prints the top 10 hottest posts listed for a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            post = response.json().get('data').get('children')
+            for i in range(10):
+                print(post[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
         print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
